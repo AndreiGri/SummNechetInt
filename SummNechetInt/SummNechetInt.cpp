28235557c68da5e8a294ba42e7b801658859cb84
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-07 20:52:46 +0500

    Добавьте файлы проекта.

---
+++ SummNechetInt/SummNechetInt.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double SummNechetInt(int num) {
+    int result = 0;
+    while (num > 0) {
+        if (num % 2 == 0) {
+            result += num;
+        }
+        num--;
+    }
+    return result;
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int number;
+    cout << "Введите натуральное число ";
+    cin >> number;
+    cout << "Сумма натуральных чётныхчисел до " << number << " составляет " << SummNechetInt(number);
+    system("pause > nul");
+    return 0;
+}
